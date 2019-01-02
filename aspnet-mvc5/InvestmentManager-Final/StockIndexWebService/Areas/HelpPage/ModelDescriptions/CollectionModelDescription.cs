5d0a53b487e867dd6fd737a133272dc91b09109a
David Berry <dberry83702@yahoo.com>
2019-01-01 20:56:06 -0600

    Removing ASP.NET MVC5 Sample

--- aspnet-mvc5/InvestmentManager-Final/StockIndexWebService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace StockIndexWebService.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- aspnet-mvc5/InvestmentManager-Initial/StockIndexWebService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StockIndexWebService.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
